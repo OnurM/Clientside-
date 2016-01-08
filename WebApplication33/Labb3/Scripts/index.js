937f63090e0be0acc96720b3b1ba3a0214ad2778
Onur <OnurM@live.com>
2016-01-08 12:28:39 +0100

    Setting up the search function on keyup

--- WebApplication33/Labb3/Scripts/index.js
+++ WebApplication33/Labb3/Scripts/index.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(document).ready(function() {
+    $("#search").keyup(function() {
+        var searchValue = $("#search").val();
+        var regexSearchValue = new RegExp(searchValue, "i");
+        $.getJSON("~/../products.json", function(data) {
+            var searchResult = "<ul class='searchResults'>";
+
+            var filteredCategory = $("input[name=product]:radio:checked").val();
+
+            $.each(data, function(key, value) {
+                if ((value.name.search(regexSearchValue)) && value.category.search()) {
+                    
+                }
+            });
+        });
+    });
+});
