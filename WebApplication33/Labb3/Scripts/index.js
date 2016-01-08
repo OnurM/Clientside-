b781adfb07892359410abb5917b5870d5b03963e
Onur <OnurM@live.com>
2016-01-08 13:22:39 +0100

    Added live filtering on radiobuttons

--- WebApplication33/Labb3/Scripts/index.js
+++ WebApplication33/Labb3/Scripts/index.js
@@ -1,17 +1,30 @@
-﻿$(document).ready(function() {
-    $("#search").keyup(function() {
-        var searchValue = $("#search").val();
-        var regexSearchValue = new RegExp(searchValue, "i");
-        $.getJSON("~/../products.json", function(data) {
-            var searchResult = "<ul class='searchResults'>";
+﻿$(document).ready(function () {
+    $("#search").keyup(function () {
+        search();
+    });
 
-            var filteredCategory = $("input[name=product]:radio:checked").val();
-
-            $.each(data, function(key, value) {
-                if ((value.name.search(regexSearchValue)) && value.category.search()) {
-                    
-                }
-            });
-        });
+    $("input[name=filterCategory]").click(function () {
+        search();
     });
 });
+
+
+
+function search() {
+    var searchValue = $("#search").val();
+    var regexSearchValue = new RegExp(searchValue, "i");
+    $.getJSON("products.json", function (data) {
+
+        var searchResult = "<ul class='searchResults'>";
+        var filteredCategory = $("input[name=filterCategory]:radio:checked").val();
+
+        $.each(data, function (key, value) {
+            if ((value.name.search(regexSearchValue) !== -1) && (value.category.search(filteredCategory) !== -1) || (value.about.search(regexSearchValue) !== -1) && (value.category.search(filteredCategory) !== -1)) {
+                searchResult += "<li> " + "<p>" + value.name + "<strong>" + value.about + "</strong> </p>" + "<img src='~/../Images/" + value.url_image + "' alt='" + value.name + "' > </li>";
+            }
+        });
+
+        searchResult += "</ul>";
+        $("#searchResult").html(searchResult);
+    });
+}
